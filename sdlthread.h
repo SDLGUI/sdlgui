86652effca5c002aa6da72787cb235c5dcd52dbb
twtfcu3 <twtfcu3@hotmail.com>
2014-06-19 21:53:50 +0800

    加入线程封装

---
+++ sdlthread.h
@@ -0,0 +1,79 @@
+﻿#ifndef __SDLGUI_THREAD_HEAD__
+#define __SDLGUI_THREAD_HEAD__
+#include <SDL2/SDL.h>
+//如果导入动态库则定义头
+#ifdef DLLEXPORT
+#define def_dll __declspec(dllexport)
+#else
+#define def_dll
+#endif
+//---------------------------------------------------
+//
+//
+//				信号对象
+//
+//
+//
+//---------------------------------------------------
+class sdl_semaphore
+{
+	public:
+		def_dll sdl_semaphore();
+		def_dll sdl_semaphore(int);
+		def_dll virtual ~sdl_semaphore();
+		/**
+		 * @brief 初始化一个信号对象
+		 *
+		 * @return 
+		 */
+		def_dll int init();
+		def_dll int init(int);
+		/**
+		 * @brief 创建一个信号对象
+		 *
+		 * @return 
+		 */
+		def_dll int create(int);
+	
+		/**
+		 * @brief 消毁信号对象
+		 *
+		 * @return 
+		 */
+		def_dll int destroy();
+		/**
+		 * @brief 等待信号
+		 *
+		 * @return 
+		 */
+		def_dll int wait();
+		/**
+		 * @brief 释放信号
+		 *
+		 * @return 
+		 */
+		def_dll int post();
+		/**
+		 * @brief 信号值
+		 *
+		 * @return 
+		 */
+		def_dll Uint32 value();
+		/**
+		 * @brief 当前信号正在等待 
+		 *
+		 * @return 
+		 */
+		def_dll int try_wait();
+		/**
+		 * @brief 等待信号超时
+		 *
+		 * @param int
+		 *
+		 * @return 
+		 */
+		def_dll int wait_timeout(Uint32);
+	protected:
+		SDL_sem* _semaphore;
+};
+#endif//__SDLGUI_THREAD_HEAD__
---
+++ sdlthread.hpp
@@ -0,0 +1,57 @@
+﻿#include <SDL2/SDL.h>
+#include "sdlthread.h"
+#ifdef __SDLGUI_THREAD_HEAD__
+def_dll sdl_semaphore::sdl_semaphore()
+{
+	init();
+}
+def_dll sdl_semaphore::sdl_semaphore(int d)
+{
+	init(d);
+}
+def_dll sdl_semaphore::~sdl_semaphore()
+{
+	destroy();
+}
+def_dll int sdl_semaphore::init()
+{
+	_semaphore = NULL;
+}
+def_dll int sdl_semaphore::init(int d)
+{
+	destroy();
+	_semaphore = SDL_CreateSemaphore(d);
+	if(_semaphore)return 0;
+	return -1;
+}
+def_dll int sdl_semaphore::destroy()
+{
+	if(_semaphore)
+	{
+		SDL_DestroySemaphore(_semaphore);
+		_semaphore = NULL;
+		return 0;
+	}
+	return -1;
+}
+def_dll int sdl_semaphore::wait()
+{
+	return SDL_SemWait(_semaphore);
+}
+def_dll int sdl_semaphore::post()
+{
+	return SDL_SemPost(_semaphore);
+}
+def_dll Uint32 sdl_semaphore::value()
+{
+	return SDL_SemValue(_semaphore);
+}
+def_dll int sdl_semaphore::try_wait()
+{
+	return SDL_SemTryWait(_semaphore);
+}
+def_dll int sdl_semaphore::wait_timeout(Uint32 ms)
+{
+	return SDL_SemWaitTimeout(_semaphore,ms);
+}
+#endif //__SDLGUI_THREAD_HEAD__
