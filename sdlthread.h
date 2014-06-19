bdc6788431894de80dd4f4194a8ab6847eff47d4
twtfcu3 <twtfcu3@hotmail.com>
2014-06-19 23:59:37 +0800

    加入线程新封装

--- sdlthread.h
+++ sdlthread.h
@@ -7,6 +7,9 @@
 #else
 #define def_dll
 #endif
+//class sdl_semaphore
+//class sdl_mutex
+//class sdl_condition
 //---------------------------------------------------
 //
 //
@@ -75,5 +78,108 @@ class sdl_semaphore
 		def_dll int wait_timeout(Uint32);
 	protected:
 		SDL_sem* _semaphore;
+};
+//---------------------------------------------------
+//
+//
+//
+//				互斥体
+//
+//
+//---------------------------------------------------
+class sdl_mutex
+{
+	friend class sdl_condition;
+	public:
+		def_dll sdl_mutex();
+		def_dll virtual ~sdl_mutex();
+		/**
+		 * @brief 创建一个互斥体
+		 *
+		 * @return 
+		 */
+		def_dll int create();
+		/**
+		 * @brief 消毁一个互斥体
+		 *
+		 * @return 
+		 */
+		def_dll int destroy();
+		/**
+		 * @brief 锁定一个互斥体
+		 *
+		 * @return 
+		 */
+		def_dll int lock();
+		/**
+		 * @brief 偿试着锁定一个互斥体
+		 *
+		 * @return 
+		 */
+		def_dll int try_lock();
+		/**
+		 * @brief 解锁一个互斥体
+		 *
+		 * @return 
+		 */
+		def_dll int unlock();
+	protected:
+		SDL_mutex* _mutex;
+};
+//---------------------------------------------------
+//
+//
+//
+//				条件变量
+//
+//
+//
+//
+//----------------------------------------------------
+class sdl_condition
+{
+	public:
+		def_dll sdl_condition();
+		def_dll virtual ~sdl_condition();
+		/**
+		 * @brief 创建一个条件变量对象
+		 *
+		 * @return 
+		 */
+		def_dll int create();
+		/**
+		 * @brief 消毁一个条件变量对象
+		 *
+		 * @return 
+		 */
+		def_dll int destroy();
+		/**
+		 * @brief 等待一个条件变量的信号
+		 *
+		 * @return 
+		 */
+		def_dll int wait(sdl_mutex*);
+		def_dll int wait(sdl_mutex&);
+		/**
+		 * @brief 在指定时间内等待一个条件变量的信号
+		 *
+		 * @return 
+		 */
+		def_dll int wait_timeout(sdl_mutex*,Uint32);
+		def_dll int wait_timeout(sdl_mutex&,Uint32);
+		/**
+		 * @brief 激动一个条件变量，让一个条件等待
+		 *
+		 * @return 
+		 */
+		def_dll int signal();
+		/**
+		 * @brief 激活所有等待线程
+		 *
+		 * @return 
+		 */
+		def_dll int broadcast();
+	protected:
+		SDL_cond* _condition;
 };
 #endif//__SDLGUI_THREAD_HEAD__
--- sdlthread.hpp
+++ sdlthread.hpp
@@ -1,6 +1,16 @@
 ﻿#include <SDL2/SDL.h>
 #include "sdlthread.h"
 #ifdef __SDLGUI_THREAD_HEAD__
+//--------------------------------------------------
+//
+//
+//
+//
+//
+//
+//
+//
+//--------------------------------------------------
 def_dll sdl_semaphore::sdl_semaphore()
 {
 	init();
@@ -53,5 +63,121 @@ def_dll int sdl_semaphore::try_wait()
 def_dll int sdl_semaphore::wait_timeout(Uint32 ms)
 {
 	return SDL_SemWaitTimeout(_semaphore,ms);
+}
+//-----------------------------------------------------------------
+//
+//
+//
+//
+//
+//					互斥体对象实现
+//
+//
+//
+///
+//-----------------------------------------------------------------
+def_dll sdl_mutex::sdl_mutex()
+{
+	_mutex = NULL;
+	create();
+}
+def_dll sdl_mutex::~sdl_mutex()
+{
+	destroy();
+}
+def_dll int sdl_mutex::create()
+{
+	destroy();
+	_mutex = SDL_CreateMutex();
+	if(_mutex)return 0;
+	return -1;
+}
+def_dll int sdl_mutex::destroy()
+{
+	if(_mutex)
+	{
+		SDL_DestroyMutex(_mutex);
+		_mutex = NULL;
+	}
+	return 0;
+}
+def_dll int sdl_mutex::lock()
+{
+	return SDL_LockMutex(_mutex);
+}
+def_dll int sdl_mutex::unlock()
+{
+	return SDL_UnlockMutex(_mutex);
+}
+def_dll int sdl_mutex::try_lock()
+{
+	return SDL_TryLockMutex(_mutex);
+}
+//----------------------------------------------------------
+//
+//
+//
+//
+//			条件变量
+//
+//
+//
+//------------------------------------------------------------
+def_dll sdl_condition::sdl_condition()
+{
+	_condition = NULL;
+	create();
+}
+def_dll sdl_condition::~sdl_condition()
+{
+	destroy();
+}
+def_dll int sdl_condition::create()
+{
+	destroy();
+	_condition = SDL_CreateCond();
+	if(_condition)return 0;
+	return -1;
+}
+def_dll int sdl_condition::destroy()
+{
+	if(_condition)
+	{
+		SDL_DestroyCond(_condition);
+		return 0;
+	}
+	return -1;
+}
+def_dll int sdl_condition::wait(sdl_mutex* p)
+{
+	if(p)
+	{
+		return SDL_CondWait(_condition,p->_mutex);
+	}
+	return -1;
+}
+def_dll int sdl_condition::wait(sdl_mutex& p)
+{
+	return SDL_CondWait(_condition,p._mutex);
+}
+def_dll int sdl_condition::wait_timeout(sdl_mutex* p,Uint32 t)
+{
+	if(p)
+	{
+		return SDL_CondWaitTimeout(_condition,p->_mutex,t);
+	}
+	return -1;
+}
+def_dll int sdl_condition::wait_timeout(sdl_mutex& p,Uint32 t)
+{
+	return SDL_CondWaitTimeout(_condition,p._mutex,t);
+}
+def_dll int sdl_condition::signal()
+{
+	return SDL_CondSignal(_condition);
+}
+def_dll int sdl_condition::broadcast()
+{
+	return SDL_CondBroadcast(_condition);
 }
 #endif //__SDLGUI_THREAD_HEAD__
